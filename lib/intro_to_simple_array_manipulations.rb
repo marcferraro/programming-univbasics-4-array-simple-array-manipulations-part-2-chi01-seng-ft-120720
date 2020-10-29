af3c51a948b8fdff5d5081cb24ca5753ac2f8410
Marc Ferraro <marc.ferraro1@gmail.com>
2020-10-29 03:42:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
-def using_concat(array1, array2)
+def using_concat (array1, array2)
   array1.concat(array2)
 end
 
@@ -6,10 +6,14 @@ def using_insert (array, element)
   array.insert(4, element)
 end
 
-def using_uniq(array)
+def using_uniq (array)
   array.uniq
 end
 
-def using_flatten(array)
+def using_flatten (array)
   array.flatten
+end
+
+def using_delete (array, string)
+  array.delete(string)
 end
